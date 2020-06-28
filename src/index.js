4fca3cd10ea4af5a4a2d7d5a892997652abc3c9a
pjchender <pjchender@gmail.com>
2020-06-28 11:09:03 +0800

    fetch data when page loaded with useEffect

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 import 'normalize.css';
 
+import * as serviceWorker from './serviceWorker';
+
+import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  //<React.StrictMode>
+    <App />,
+  //</React.StrictMode>,
   document.getElementById('root')
 );
 
